e4c79753d69de2db79bc08c1e4e4e4c65d877d77
NeewLife <dhtpdud2009@gmail.com>
2022-11-18 18:01:29 +0900

    세영 카드추가+연결시도

--- final/src/main/java/com/study/domain/resttime/ResttimeController.java
+++ final/src/main/java/com/study/domain/resttime/ResttimeController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("info")
 @RequiredArgsConstructor
 public class ResttimeController {
 
