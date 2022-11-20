4c1e446b3e118f4bb9e7de8194810c1eecd36550
Neewlife <dhtpdud2009@gmail.com>
2022-11-20 18:23:50 +0900

    1120

--- final/src/main/java/com/study/domain/resttime/ResttimeController.java
+++ final/src/main/java/com/study/domain/resttime/ResttimeController.java
@@ -1,11 +1,14 @@
 package com.study.domain.resttime;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@SessionAttributes("info")
 @RequiredArgsConstructor
 public class ResttimeController {
 
