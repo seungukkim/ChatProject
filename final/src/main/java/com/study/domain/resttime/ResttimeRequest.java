dece3edf2065c86d232087ac95e34b616412e277
seungukkim <gogobattle@naver.com>
2022-11-29 14:16:18 +0900

    원상복구

---
+++ final/src/main/java/com/study/domain/resttime/ResttimeRequest.java
@@ -0,0 +1,13 @@
+package com.study.domain.resttime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ResttimeRequest {
+	private String userMbti;
+	private Integer randomNum;
+}
---
+++ final/src/main/java/com/study/domain/resttime/ResttimeResponse.java
@@ -0,0 +1,9 @@
+package com.study.domain.resttime;
+
+import lombok.Getter;
+
+@Getter
+public class ResttimeResponse {
+	private String getRestAsMbti;
+	private Integer randomNum;
+}
