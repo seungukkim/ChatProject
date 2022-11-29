7ae612e3211278f7f47707a17915591902f673c4
seungukkim <gogobattle@naver.com>
2022-11-29 17:20:28 +0900

    update

--- final/src/main/java/com/study/domain/resttime/ResttimeController.java
+++ final/src/main/java/com/study/domain/resttime/ResttimeController.java
@@ -1,7 +1,11 @@
 package com.study.domain.resttime;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.RequiredArgsConstructor;
@@ -13,17 +17,14 @@ public class ResttimeController {
 
     private final ResttimeService resttimeService;
 
-	/*
-	 * @PostMapping("/resttime/resttime/")
-	 * 
-	 * @ResponseBody public String getRestAsMbti(final ResttimeRequest params,
-	 * HttpServletRequest request) {
-	 * System.out.println("resttime/resttime 컨트롤러 진입. 넘어온 파라미터 : "+params);
-	 * 
-	 * 
-	 * String method = resttimeService.getRestAsMbti(params); //유저의 mbti를 통해 휴식방법을
-	 * db로부터 가져오기 System.out.println(method); return method; }
-	 */
+	
+	 @PostMapping("/resttime/resttime/")
+	 @ResponseBody public String getRestAsMbti(final ResttimeRequest params, HttpServletRequest request) {
+	 System.out.println("resttime/resttime 컨트롤러 진입. 넘어온 파라미터 : "+params);    
+	 String method = resttimeService.getRestAsMbti(params); //유저의 mbti를 통해 휴식방법을
+	 System.out.println(method); return method; 
+	 }
+	 
     
     @GetMapping("/resttime/resttime")
     public String hihi() {
--- final/src/main/java/com/study/domain/resttime/ResttimeMapper.java
+++ final/src/main/java/com/study/domain/resttime/ResttimeMapper.java
@@ -1,5 +1,8 @@
 package com.study.domain.resttime;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ResttimeMapper {
 	
 	/* List<String> restAsMbti */
