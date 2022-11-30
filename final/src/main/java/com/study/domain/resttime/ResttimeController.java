bdccdee9aaf1d8c8a0f97667c0836cbbccbf69a0
NeewLife <dhtpdud2009@gmail.com>
2022-11-30 11:19:24 +0900

    원본

--- final/src/main/java/com/study/domain/resttime/ResttimeController.java
+++ final/src/main/java/com/study/domain/resttime/ResttimeController.java
@@ -1,19 +1,12 @@
 package com.study.domain.resttime;
 
-<<<<<<< HEAD
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-=======
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> origin/css수정
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.RequiredArgsConstructor;
